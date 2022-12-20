ae0f21fbf449b9777f786e9d642041dc077847d4
stackman27 <sis1001@berkeley.edu>
2022-12-20 15:10:10 -0800

    working on test

--- x/lockup/keeper/msg_server_test.go
+++ x/lockup/keeper/msg_server_test.go
@@ -1,6 +1,7 @@
 package keeper_test
 
 import (
+	"fmt"
 	"time"
 
 	gammtypes "github.com/osmosis-labs/osmosis/v13/x/gamm/types"
@@ -423,6 +424,7 @@ func (suite *KeeperTestSuite) TestMsgForceUnlock() {
 		suite.FundAcc(addr1, coinsToLock)
 
 		unbondingDuration := suite.App.StakingKeeper.GetParams(suite.Ctx).UnbondingTime
+		fmt.Println(unbondingDuration)
 		resp, err := msgServer.LockTokens(c, types.NewMsgLockTokens(addr1, unbondingDuration, coinsToLock))
 		suite.Require().NoError(err)
 
